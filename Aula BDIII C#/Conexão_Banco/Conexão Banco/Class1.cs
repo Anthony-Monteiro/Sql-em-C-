c503275c1628276883270054754a5b19db3d537b
TonhaoGameplays <128548364+TonhaoGameplays@users.noreply.github.com>
2023-03-31 20:23:43 -0300

    Add files via upload

---
+++ Aula BDIII C#/Conexão_Banco/Conexão Banco/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexão_Banco
+{
+    class Conecta
+    {
+        public static MysqlConnection con = new MysqlConnection
+            ("server=dspwiii.mysql.dbaas.com.br; database=dspwiii; user=dspwiii; password=Dspw3@");
+
+        public void Abrir()
+        {
+            con.Open();
+        }
+
+
+    }
+}
